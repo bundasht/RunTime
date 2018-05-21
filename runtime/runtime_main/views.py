e5a6dfdba35207540adc08475064b84e728e35f2
Bohdan Turani <bodya00@list.ru>
2018-05-21 23:19:33 +0200

    added model for ML

---
+++ runtime/runtime_main/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def register(request):
+    return render(request, 'register.html')
+def login(request):
+    return render(request, 'login.html')
